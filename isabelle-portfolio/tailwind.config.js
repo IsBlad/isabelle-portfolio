4198410937ab40236c73b96ce4087d3f49223de5
IsabelleR <isabellerandeblad@gmail.com>
2025-01-13 23:12:28 +1100

    add custom colors and fonts

--- isabelle-portfolio/tailwind.config.js
+++ isabelle-portfolio/tailwind.config.js
@@ -10,11 +10,17 @@ module.exports = {
         darkGreen: '#1C3E40',
         darkerGreen: '#132828',
         midGreen: '#314E50',
+        link: '#A3A597',
         lightGreen: '#C9CCBD',
+        bg: '#EEEFE8',
+        inner:'#F9F9F9',
         pink: '#DABEA7',
         midPink: '#EFD0BF',
         lightPink: '#F4E7D7',
       },
+      fontFamily: {
+        sans: ['Poppins', 'Arial', 'sans-serif'],
+      }
     },
   },
   plugins: [],
