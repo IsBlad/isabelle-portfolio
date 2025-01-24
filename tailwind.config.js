3c8417171d61fcc254840c392cd3c475751cc5f7
IsabelleR <isabellerandeblad@gmail.com>
2025-01-24 21:53:38 +1100

    add custon font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,18 @@ module.exports = {
         sans: ['Poppins', 'Arial', 'sans-serif'],
         lime: ['Limelight', 'sans-serif', 'Arial'],
         quick: ['Quicksand', 'sans-serif', 'Arial'],
-      }
+      },
+      fontSize: {
+        // Add custom text sizes here
+        '4xl': '2.25rem',  // 36px
+        '5xl': '3rem',     // 48px
+        '5.5xl': '3.25rem',
+        '6xl': '3.75rem',  // 60px
+        '7xl': '4.5rem',   // 72px
+        '8xl': '6rem',     // 96px
+        '9xl': '8rem',     // 128px
+        'huge': '10rem',   // Custom: 160px
+      },
     },
   },
   plugins: [],
